9f6d9e8783595e7344ada8af770b98ce010abee8
Devansh Raghuvanshi <64578916+devanshraghav@users.noreply.github.com>
2024-07-09 10:28:24 +0530

    Auto typing and form alert added.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const hamburger = document.getElementById("hamburger");
 const menu = document.querySelector(".menu");
 
+const form = document.querySelector("form");
+const inputField = document.querySelectorAll('.input-field');
 
 hamburger.addEventListener("click", function () {
   const hamIcon = this.querySelector(".hamburger-icon");
@@ -15,4 +17,10 @@ hamburger.addEventListener("click", function () {
     crossIcon.style.display = "inline-block";
     menu.style.display = "block";
   }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  inputField.forEach((input)=> input.value = '')
+  alert("Under Maintenance");
 });
